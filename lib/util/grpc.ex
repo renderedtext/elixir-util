48868706a930b9214258fb6da64a436608ab0f57
Igor Šarčević <igor@renderedtext.com>
2022-04-07 17:12:47 +0000

    Refinments for the API

--- lib/util/grpc.ex
+++ lib/util/grpc.ex
@@ -1,50 +1,45 @@
 defmodule Util.Grpc do
+  @moduledoc """
+  This module provides an opiniated interface for communicating with
+  GRPC services. It handles starting and closing connections, logging,
+  metrics, and proper error handling.
+
+  Usage:
+
+  1. First, add Util.Grpc to your application's supervision tree.
+     This process keeps the configuration values for your Grpc clients.
+
+     children = [
+       worker(Util.Grpc, [])
+     ]
+
+     Supervisor.start_link(children, opts)
+
+  2. Describe the outgoing connections from your service:
+
+     Util.Grpc.setup([
+       Util.Grpc.Client.new(:user_service, "localhost:50051", UserApi.Stub),
+       Util.Grpc.Client.new(:billing_service, "localhost:50051", BillingApi.Stub)
+     ])
+
+  3. User Grpc.do to communicate with your upstream services:
+
+    req = ExampleApi.DescribeRequest.new(name: "a")
+
+    {:ok, res} = Util.Grpc.call(:user_service, :describe, req)
+
+  During the execution of the call, the following metrics are published:
+
+    - gprc.<client_name>.<method_name>.duration
+    - gprc.<client_name>.<method_name>.connect
+    - gprc.<client_name>.<method_name>.connect.error.count
+    - gprc.<client_name>.<method_name>.response.success.count
+    - gprc.<client_name>.<method_name>.response.error.count
+
+  In case of errors, log messages are logged via the Logger module.
+  """
 
   @type client_name :: String.t()
-
-  defmodule Client do
-    defstruct :name, :endpoint, :timeout, :log_level, :publish_metrics
-
-    @type t() :: {
-      name: String.t(),
-      endpoint: String.t(),
-      timeout: number(),
-      log_level: atom(),
-      publish_metrics: boolean()
-    }
-
-    @default_timeout 30_000 # 30 seconds
-
-    def new(name, endpoint), do: new(name, endpoint, @default_timeout, :info, true)
-    def new(name, endpoint, timeout), do: new(name, endpoint, timeout, :info, true)
-
-    def new(name, endpoint, timeout, log_level, publish_metrics) do
-      %__MODULE__{
-        name: name,
-        endpoint: endpoint,
-        timeout: timeout,
-        log_level: log_level,
-        publish_metrics: publish_metrics
-      }
-    end
-
-    def do(client, callback) do
-      Wormhole.capture(fn ->
-        Watchman.benchmark("grpc.#{client.name}.duration", fn ->
-          Watchman.increment("grpc.#{client.name}.connect")
-
-          case Grpc.Stub.connect(client.endpoint) do
-            {:ok, channel} ->
-              callback.(channel)
-
-            {:error, error} ->
-              Logger.log(client.level, "Failed to connect to #{client.name} service err='#{inspect(error)}'")
-              Watchman.increment("grpc.#{client.name}.connect.error")
-          end
-        end)
-      end)
-    end
-  end
 
   def start_link do
     Util.Grpc.State.start_link()
@@ -69,7 +64,6 @@ defmodule Util.Grpc do
     :ok
   end
 
-  @spec rpc(client_name(), GRPC.Channel.t(), function()) :: {:ok, any()} | {:error, :failed_to_connect, any()} | {:error, :timeout, any()} | {:error, any()}
   @doc """
   Executes a stateless RPC call to a remote service.
 
@@ -77,23 +71,14 @@ defmodule Util.Grpc do
   2. Sends the RPC request
   3. Waits for the result, or times out
   """
-  def do(client_name, callback) do
-    {:ok, client} = Agent.find(client_name)
+  @spec rpc(client_name(), GRPC.Channel.t(), function()) ::
+          {:ok, any()}
+          | {:error, :failed_to_connect, any()}
+          | {:error, :timeout, any()}
+          | {:error, any()}
+  def call(client_name, callback) do
+    {:ok, client} = State.find_client(client_name)
 
     Client.rpc(client, callback)
-  end
-
-  end
-
-  defmodule State do
-    use Agent
-
-    def start_link do
-      Agent.start_link(fn -> %{} end, name: __MODULE__)
-    end
-
-    def reset do
-      Agent.update(__MODULE__, %{})
-    end
   end
 end
